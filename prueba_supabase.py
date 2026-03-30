1328a2609858503e6a7a40f8ccd902acebbff515
Johnny Marcel Euceda Gutiérrez <johnny.euceda@somoscmi.com>
2026-03-30 12:57:11 -0600

    Poniendo seguridad

--- prueba_supabase.py
+++ prueba_supabase.py
@@ -1,12 +1,22 @@
+import os
+from dotenv import load_dotenv
 from supabase import create_client
 
-url = "https://aqkzoxuzhoqueferheql.supabase.co"
-key = "sb_publishable_-33um-_x5kjNd7dJM7tESw_9lHlK94x"
+# Carga las variables del archivo .env
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
 supabase = create_client(url, key)
+# ... el resto de tu código de insertar() sigue igual
+
+from supabase import create_client
+
 
 def insertar():
     print("Intentando conectar con Supabase...")
-    res = supabase.table("pruebas").insert({"nombre": "Marcel desde Docker"}).execute()
+    res = supabase.table("pruebas").insert({"nombre": "Marcel"}).execute()
     print("¡Respuesta de la base de datos!", res.data)
 
 if __name__ == "__main__":
